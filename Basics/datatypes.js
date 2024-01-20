fb16942c6e9b7ca2f2f96dd383d42ecf332c34e2
GoddamSam <155828538+GoddamSam@users.noreply.github.com>
2024-01-20 13:16:44 +0530

    files added

---
+++ Basics/datatypes.js
@@ -0,0 +1,7 @@
+//six primitive data types-->number,string,boolean,null,undefined,symbol 
+
+//typeof function tells the type of the value
+
+console.table([typeof 5,typeof 'Sam',typeof true,typeof null,typeof undefined])
+
+//type of null is object whereas type of undefined is undefined....this is a instance of debate where some say it is a bug in the language while some say this is the norm
