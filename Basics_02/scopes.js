fb6219178b5d2eda9178eb9e0d18708a55ef6ef9
csamaroho <csamaroho060602@gmail.com>
2024-01-25 00:29:38 +0530

    files added

--- Basics_02/scopes.js
+++ Basics_02/scopes.js
@@ -22,3 +22,19 @@ if(true){
 //since, var types are always globally scoped using them inside blocks becomes difficult since they get updated if variable names are same and pose problems i.e, let and const types are prefered
 
 //***Note***-->the scopes are different in node enviorment and browser's execution enviorment
+
+//Hoisting-->
+
+console.log(addone(2,3))
+function addone(a,b)
+{
+    return a+b
+}
+//In this type of function declaration hoisting takes place so calling a function even before it is declared works fine
+
+
+console.log(sum(2,3))
+let sum=function addtwo(a,b){
+    return a+b
+}
+//In this type of declaration the hoisting dosen't work so error is given if the function is accessed before initialization
